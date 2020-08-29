2447f0f4f658a7cfb530f28fe4b1994c1825cf70
SAMUEL PETERS <u1275691@umail.utah.edu>
2020-08-29 09:43:39 -0600

    Setup for solution and project for formula evaluator

---
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FormulaEvaluator
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
