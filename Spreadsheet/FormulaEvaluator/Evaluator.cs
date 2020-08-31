0bbab7f3c1553cde74c8a01c91ad8ccb9d3dc952
SAMUEL PETERS <u1275691@umail.utah.edu>
2020-08-31 15:57:49 -0600

    Began implementing if statements to complete algorithm

--- Spreadsheet/FormulaEvaluator/Evaluator.cs
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace FormulaEvaluator
@@ -16,8 +17,6 @@ namespace FormulaEvaluator
         /// <returns>Int value of the variable. Throws ArgumentException if the variable has no value.</returns>
         public delegate int Lookup(string v);
 
-        //Test
-
         /// <summary>
         /// Takes in a string and delegate to find the value of the expression
         /// </summary>
@@ -30,11 +29,117 @@ namespace FormulaEvaluator
             string[] tokens = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
             Stack<int> values = new Stack<int>();
             Stack<char> operators = new Stack<char>();
+            char tempChar;
+            int tempInt;
 
             //Goes through tokens to evaluate expression
             foreach(string s in tokens)
             {
+                //If s is an int
+                if (int.TryParse(s, out tempInt))
+                {
+                    int num2 = tempInt;
+                    //If * or / is on operator stack
+                    if (operators.TryPeek(out char c) && (c == '*' || c == '/'))
+                    {
+                        operators.Pop();
+                        //Pop value and do operand
+                        if (values.TryPop(out int num1))
+                        {
+                            if (c == '/' && num1 == 0)
+                                throw new ArgumentException("Dividing by 0.");
+                            else
+                            {
+                                if (c == '/')
+                                    values.Push(num1 / num2);
+                                else
+                                    values.Push(num1 * num2);
+                            }
+                        }
+                        else
+                            throw new ArgumentException("Adding or dividing with empty value stack.");
+                    }
+                    //Otherwise, just add to values stack
+                    else
+                        values.Push(num2);
+                }
 
+                //If s is variable
+                else if (Regex.IsMatch(s, @"^[a-zA-Z]+$"))
+                {
+                    int num2;
+                    //Check if var exists
+                    try
+                    {
+                        num2 = variableEvaluator(s);
+                    }
+                    catch
+                    {
+                        throw new ArgumentException("Value for variable " + s + " cannot be found.");
+                    }
+
+                    //If * or / is on operator stack
+                    if (operators.TryPeek(out char c) && (c == '*' || c == '/'))
+                    {
+                        operators.Pop();
+                        //Pop value and do operand
+                        if (values.TryPop(out int num1))
+                        {
+                            if (c == '/' && num1 == 0)
+                                throw new ArgumentException("Dividing by 0.");
+                            else
+                            {
+                                if (c == '/')
+                                    values.Push(num1 / num2);
+                                else
+                                    values.Push(num1 * num2);
+                            }
+                        }
+                        else
+                            throw new ArgumentException("Adding or dividing with empty value stack.");
+                    }
+                    //Otherwise, just add to values stack
+                    else
+                        values.Push(num2);
+                }
+
+                //If s is + or -
+                else if (char.TryParse(s, out tempChar) && (tempChar == '+' || tempChar == '-'))
+                {
+                    char c2 = tempChar;
+
+                    //If + or - on operator stack
+                    if (operators.TryPeek(out char c1) && (c1 == '+' || c1 == '-'))
+                    {
+                        //If 2+ values exist in values stack
+                        if (values.Count > 1)
+                        {
+                            int num2 = values.Pop();
+                            int num1 = values.Pop();
+                            if (c1 == '+')
+                                values.Push(num1 + num2);
+                            else
+                                values.Push(num1 - num2);
+                        }
+                        else
+                            throw new ArgumentException("Attempting to add or subtract with less than 2 values in the values stack.");
+                    }
+                    operators.Push(c2);
+                }
+
+                //If s is * or /
+                else if (char.TryParse(s, out tempChar) && (tempChar == '*' || tempChar == '/'))
+                    operators.Push(tempChar);
+
+                //If s is (
+                else if (char.TryParse(s, out tempChar) && tempChar == ')')
+                    operators.Push(tempChar);
+
+                //If s is )
+                else if (char.TryParse(s, out tempChar) && tempChar == '(')
+                {
+                    //TODO
+                }
             }
 
             return 0;
