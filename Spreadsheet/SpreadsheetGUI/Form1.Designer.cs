b763e165d71ed0cb0144c1504ba9a53fa1f95115
SAMUEL PETERS <u1275691@umail.utah.edu>
2020-10-03 15:48:27 -0600

    Set up for ps6

---
+++ Spreadsheet/SpreadsheetGUI/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SpreadsheetGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cellsPanel = new SS.SpreadsheetPanel();
+            this.SuspendLayout();
+            // 
+            // cellsPanel
+            // 
+            this.cellsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cellsPanel.Location = new System.Drawing.Point(12, 12);
+            this.cellsPanel.Name = "cellsPanel";
+            this.cellsPanel.Size = new System.Drawing.Size(776, 426);
+            this.cellsPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cellsPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SS.SpreadsheetPanel cellsPanel;
+    }
+}
+
